e38777f474ae3f6f5a2f61efad6b5aa095e7d6a5
Daarrik <darrikwh@gmail.com>
2022-02-01 02:32:52 -0800

    oh my god it works
    but whyyyyy is a 'plugin' needed for this

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,15 @@
 import React, { useContext } from "react";
 import { LanguageContext } from "../components/LanguageProvider";
-import Layout from "../components/layout";
 import Blob from "../components/blob";
 
 const IndexPage = () => {
-  // const { language } = useContext(LanguageContext); // language is undefined (??)
+  const { language } = useContext(LanguageContext); // language is undefined (??)
 
   return (
-    // CAN'T HAVE LAYOUT AS PARENT. CONTEXT MUST BE PASSED DOWN VIA ROOT ELEMENT
-    // STILL FINDING ROOT ELEMENT
-    <Layout>
-      <main className="main center">
-        <Blob />
-      </main>
-    </Layout>
+    <main className="main center">
+      <Blob />
+      {language.main}
+    </main>
   );
 };
 
